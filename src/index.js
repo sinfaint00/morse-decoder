7a3747c5fb971a73789d64cf7f99486bb2abbb96
sinfaint00 <122840916+sinfaint00@users.noreply.github.com>
2023-08-15 20:53:40 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,45 +1,54 @@
-const MORSE_TABLE = {
-    '1011': 'a',
-    '11101010': 'b',
-    '11101110': 'c',
-    '1101010': 'd',
-    '10': 'e',
-    '10101110': 'f',
-    '11011110': 'g',
-    '10101010': 'h',
-    '1000': 'i',
-    '10111111': 'j',
-    '1101011': 'k',
-    '10111010': 'l',
-    '11110': 'm',
-    '1110': 'n',
-    '11111': 'o',
-    '10111110': 'p',
-    '11111011': 'q',
-    '1001010': 'r',
-    '1001010': 's',
-    '1000': 't',
-    '1001011': 'u',
-    '10101011': 'v',
-    '1001111': 'w',
-    '11101011': 'x',
-    '11101111': 'y',
-    '11111010': 'z',
-    '11111111': ' ',
-};
+function decodeMorseCode(input) {
+    // Define a dictionary for Morse code mapping
+    const morseCodeMap = {
+        '0000001011': 'a',
+        '0011101010': 'b',
+        '0011101110': 'c',
+        '0000111010': 'd',
+        '0000000010': 'e',
+        '0010101110': 'f',
+        '0000111110': 'g',
+        '0010101010': 'h',
+        '0000001010': 'i',
+        '0010111111': 'j',
+        '0000111011': 'k',
+        '0010111010': 'l',
+        '0000001111': 'm',
+        '0000001110': 'n',
+        '0000111111': 'o',
+        '0010111110': 'p',
+        '0011111011': 'q',
+        '0000101010': 'r',
+        '0000101010': 's',
+        '0000001000': 't',
+        '0000101011': 'u',
+        '0010101011': 'v',
+        '0000101111': 'w',
+        '0011101011': 'x',
+        '0011101111': 'y',
+        '0011111010': 'z',
+        '1011111111': ' ',
+    };
+  
+    const encodedLetters = input.match(/.{10}/g);
 
-function decode(expr) {
-    let decoded = '';
-
-    for (let i = 0; i < expr.length; i += 10) {
-        const letterCode = expr.substr(i, 10);
-        const morseCode = letterCode.replace(/10/g, '.').replace(/11/g, '-').replace(/0/g, '');
-        decoded += MORSE_TABLE[morseCode];
-    }
-
-    return decoded;
-}
-
-module.exports = {
-    decode
-};
+    // Decode each letter and store the results in an array
+    const decodedLetters = encodedLetters.map(encodedLetter => {
+      if (encodedLetter === '**********') {
+        return ' ';
+      } else {
+        const decodedChar = morseCodeMap[encodedLetter];
+        return decodedChar || '?'; // Handle unknown characters
+      }
+    });
+  
+    // Join the decoded letters to form the final decoded string
+    const decodedString = decodedLetters.join('');
+  
+    return decodedString;
+  }
+  
+  // Test the function with an example input
+  const input = "00000011110000000010"; // Encoded "me"
+  const decodedOutput = decodeMorseCode(input);
+  console.log(decodedOutput); // Output: "me"
